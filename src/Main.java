518fce391eebd5465f6e1df5db84f1640178ab91
Daniel Caetano <daniel.caetano@univille.br>
2026-09-15 15:05:27 -0300

    Refatoramento do código

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+     public static void main(String[] args){
+
+        Refatorar Maquina1 = new Refatorar("Brastemp");
+
+        Maquina1.ligar();
+
+        Maquina1.ligar();
+     }
 }
