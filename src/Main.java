10857d156427142b28214eb02def8b7b84cc0f41
Daniel Caetano <daniel.caetano@univille.br>
2026-09-15 15:48:35 -0300

    Instanciando Objetos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 public class Main {
      public static void main(String[] args){
 
-        Refatorar Maquina1 = new Refatorar("Brastemp");
-
-        Maquina1.ligar();
-
-        Maquina1.ligar();
+        MaquinaDeLavar maquina1 = new MaquinaDeLavar("Brastemp BWK11");
+        maquina1.exibirStatus();
+ 
+        maquina1.ligar();
+        maquina1.fecharTampa();
+        maquina1.iniciarLavagem();
+        maquina1.exibirStatus();
+ 
+        maquina1.concluirLavagem();
+        maquina1.iniciarCentrifugacao();
+        maquina1.exibirStatus();
+ 
+        maquina1.concluirCentrifugacao();
+        maquina1.abrirTampa();
+        maquina1.desligar();
+        maquina1.exibirStatus();
+ 
+        System.out.println();
+        System.out.println("Máquina 2: tentativas inválidas");
+        MaquinaDeLavar maquina2 = new MaquinaDeLavar("Electrolux LAC12");
+        maquina2.exibirStatus();
      }
 }
